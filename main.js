e2ee8e54704e4a9bdd5c8b098fc310ae535ff9da
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2023-03-17 21:31:02 +0900

    修正

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ import { reactive, effect } from "./reactivity.js";
 const obj1 = reactive({ a: 0 });
 const obj2 = reactive({ a: 0 });
 
-console.log(obj1.a + 100);
-
-// 現状、obj1.bを呼び出した場合でも以下の処理が呼び出されてしまう
+// 現状、obj1.aを呼び出した場合でも以下の処理が呼び出されてしまう
 // 本来はリアクティブな値（obj2.a）が更新された時のみ呼び出したい
 effect(() => {
-  const value = obj1.a + 100;
-  console.log(value);
+  console.log("1", obj1.a);
+});
+
+effect(() => {
+  console.log("2", obj2.a);
 });
 
 obj1.a = 1;
--- reactivity.js
+++ reactivity.js
@@ -1,12 +1,13 @@
 const handler = {
   get: function (target, prop, receiver) {
     console.log("get trap called");
+    track(target);
     return Reflect.get(target, prop, receiver);
   },
   set: function (target, prop, value, receiver) {
     Reflect.set(target, prop, value, receiver);
     console.log("set trap called");
-    trigger();
+    trigger(target);
     return true;
   },
 };
@@ -20,11 +21,23 @@ let activeEffect = null;
 // リアクティブなオブジェクトの変更時に呼ばれるメソッドを保存する
 const effect = (fn) => {
   activeEffect = fn;
-};
-
-// リアクティブなオブジェクトの変更を検知
-const trigger = () => {
   activeEffect();
 };
 
+const targetMap = new WeakMap();
+
+const track = (target) => {
+  targetMap.set(target, activeEffect);
+};
+
+// リアクティブなオブジェクトの変更を検知
+const trigger = (target) => {
+  const effect = targetMap.get(target);
+  effect();
+};
+
 export { reactive, effect };
+
+// effectで関数を登録すると、一度その関数を実行する
+// 実行した関数内にリアクティブなオブジェクトがあればゲッターが呼ばれる
+// ゲッターの中でリアクティブなオブジェクトと登録した関数の紐付けを行う
