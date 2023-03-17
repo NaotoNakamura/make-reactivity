faa28696644b8518c74511f0de3f10c373688578
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2023-03-17 21:41:44 +0900

    呼び出し側の処理を修正

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 import { reactive, effect } from "./reactivity.js";
-const obj1 = reactive({ a: 0 });
-const obj2 = reactive({ a: 0 });
+const obj1 = reactive({
+  a: 0,
+  b: 0,
+});
 
-// 現状、obj1.aを呼び出した場合でも以下の処理が呼び出されてしまう
-// 本来はリアクティブな値（obj2.a）が更新された時のみ呼び出したい
+// 現状、obj1.bを呼び出した場合でもobj1.aにしか依存してないeffectの処理が呼び出されてしまう
+// （現状はWeakMapに登録した関数が上書きされるので、一番最後にeffectに登録したものが実行）
+// 本来はリアクティブな値（obj1.a）が更新された時のみ呼び出したい
 effect(() => {
-  console.log("1", obj1.a);
+  console.log("a", obj1.a);
 });
 
 effect(() => {
-  console.log("2", obj2.a);
+  console.log("b", obj1.b);
 });
 
-obj1.a = 1;
-obj2.a = 1;
+obj1.b = 1;
