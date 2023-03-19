19e0d357ab91013a85cce5afeb40f131ac74b299
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2023-03-19 15:50:34 +0900

    同一オブジェクトの同一プロパティに対して複数のeffectを登録できるようにした

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 import { reactive, effect } from "./reactivity.js";
 const obj1 = reactive({
-  a: 0,
-  b: 0,
-});
-
-// 現状、1つのオブジェクトのプロパティに対して、1つの関数しか登録できない
-effect(() => {
-  console.log("a", obj1.a);
+  a: 1,
+  b: 2,
 });
 
 effect(() => {
-  console.log("b", obj1.b);
+  console.log(obj1.a * 2);
 });
 
-obj1.b = 1;
+// getの時はそのまま値を返す
+console.log(obj1.a);
+
+// setの時（オブジェクトが変更された時）は
+// オブジェクトのプロパティに対応したeffectを呼び出して実行する
+obj1.a = 1;
--- reactivity.js
+++ reactivity.js
@@ -22,34 +22,51 @@ let activeEffect = null;
 const effect = (fn) => {
   activeEffect = fn;
   activeEffect();
+  activeEffect = null;
 };
 
+// リアクティブオブジェクト: Mapオブジェクト
+// （{{a: 0, b: 0} => Map(0) {…}}）
+// オブジェクト: Map(オブジェクトのプロパティ名: [実行したい関数])
 const targetMap = new WeakMap();
 
 const track = (target, key) => {
+  if (activeEffect === null) return;
   let depsMap = targetMap.get(target);
   if (depsMap === undefined) {
+    // リアクティブオブジェクトのキー名: Setオブジェクト
+    // a => Set(0) {…}
     depsMap = new Map();
     targetMap.set(target, depsMap);
   }
-  // key（オブジェクトのプロパティ）
-  // activeEffect（effectで登録した関数）
-  depsMap.set(key, activeEffect);
+  let deps = depsMap.get(key);
+  if (deps === undefined) {
+    // 同一オブジェクトの同一プロパティに対して複数の関数を登録できるようにSetを使用
+    deps = new Set();
+    depsMap.set(key, deps);
+  }
+  if (!deps.has(activeEffect)) {
+    deps.add(activeEffect);
+  }
 };
 
 // リアクティブなオブジェクトの変更を検知
 const trigger = (target, key) => {
   const depsMap = targetMap.get(target);
-  if (depsMap === undefined) {
-    return;
-  }
-  const effect = depsMap.get(key);
-  effect();
+  if (depsMap === undefined) return;
+  const deps = depsMap.get(key);
+  if (deps === undefined) return;
+  deps.forEach((effect) => {
+    effect();
+  });
 };
 
 export { reactive, effect };
 
 // effectで関数を登録すると、一度その関数を実行する
 // 実行した関数内にリアクティブなオブジェクトがあればゲッターが呼ばれる
-// ゲッターの中でリアクティブなオブジェクトをキーに、
-// プロパティとeffectで登録した関数の組み合わせをバリューにして
+// ゲッターの中ではtrackが呼び出され、リアクティブなオブジェクトをキーにMapを取得
+// 取得したMapがundefinedであれば、リアクティブなオブジェクトをキーに空のMapを登録
+// 取得したMapに対して、プロパティ名をキーに配列を取得
+// 取得した配列がundefinedであれば、プロパティ名をキーに空のSetを登録
+// 取得した配列に現在実行しているeffectがなければ追加する
