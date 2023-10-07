87aea7202a259ba3ff304d1c0fe53762eb56361c
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2023-10-07 15:10:19 +0900

    render内のHTMLを修正

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ import { createApp, h } from "./app.js";
 
 createApp({
   render() {
-    return h("div", {}, "hello");
+    return h("div", { class: "container" }, [
+      h("div", { class: "num" }, [
+        h("button", { class: "btn" }, "+"),
+        h("div", { class: "label" }, 8),
+        h("button", { class: "btn" }, "-"),
+      ]),
+      h("num", { class: "num" }, "+"),
+      h("div", { class: "num" }, [
+        h("button", { class: "btn" }, "+"),
+        h("div", { class: "label" }, 7),
+        h("button", { class: "btn" }, "-"),
+      ]),
+      h("div", { class: "num" }, "="),
+      h("div", { class: "result" }, 15),
+    ]);
   },
 }).mount("#app");
